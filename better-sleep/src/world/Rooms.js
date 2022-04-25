3114ce772f6600651df81b5dbb9ea510376e1835
Marcel Stolin <mstolin@mailbox.org>
2022-04-25 14:39:26 +0200

    Added lights to the rooms

--- better-sleep/src/world/Rooms.js
+++ better-sleep/src/world/Rooms.js
@@ -1,4 +1,5 @@
 const Room = require('../classes/Room')
+const Light = require('../classes/Light')
 
 const ID_ROOM_FIRST_FLOOR = 'first_floor'
 const ID_ROOM_LIVING_ROOM = 'living_room'
@@ -8,53 +9,66 @@ const ID_ROOM_GUESTROOM = 'guestroom'
 const ID_ROOM_SECOND_FLOOR = 'second_floor'
 const ID_ROOM_BATHROOM = 'bathroom'
 
+const ID_LIGHT_MAIN = 'main-light'
+const ID_LIGHT_SMALL_1 = 'small-light-1'
+const ID_LIGHT_SMALL_2 = 'small-light-2'
+
+// Light profiles, since most rooms have the same
+let lightProfile1 = {}
+lightProfile1[ID_LIGHT_MAIN] = new Light('Main Light')
+
+let lightProfile2 = {}
+lightProfile2[ID_LIGHT_MAIN] = new Light('Main Light')
+lightProfile2[ID_LIGHT_SMALL_1] = new Light('Small Light 1')
+lightProfile2[ID_LIGHT_SMALL_2] = new Light('Small Light 2')
+
 // Lower floor
 let firstFloor = new Room(
     'First Floor',
     [ID_ROOM_LIVING_ROOM, ID_ROOM_KITCHEN, ID_ROOM_FIRST_FLOOR],
     {},
-    {}
+    lightProfile1
 )
 let livingRoom = new Room(
     'Living Room',
     [ID_ROOM_KITCHEN, ID_ROOM_FIRST_FLOOR],
     {},
-    {}
+    lightProfile2
 )
 let kitchen = new Room(
     'Kitchen',
     [ID_ROOM_LIVING_ROOM, ID_ROOM_FIRST_FLOOR],
     {},
-    {}
+    lightProfile2
 )
 // Upper floor
 let bedroom = new Room(
     'Bedroom',
     [ID_ROOM_SECOND_FLOOR],
     {},
-    {}
+    lightProfile2
 )
 let guestroom = new Room(
     'Guestroom',
     [ID_ROOM_SECOND_FLOOR],
     {},
-    {}
+    lightProfile2
 )
 let secondFloor = new Room(
     'Second Floor',
     [ID_ROOM_BEDROOM, ID_ROOM_BATHROOM, ID_ROOM_GUESTROOM, ID_ROOM_FIRST_FLOOR],
     {},
-    {}
+    lightProfile1
 )
 let bathroom = new Room(
     'Bathroom',
     [ID_ROOM_SECOND_FLOOR],
     {},
-    {}
+    lightProfile1
 )
 
 // exports identifier
-module.exports.identifier = {
+module.exports.roomIds = {
     ID_ROOM_FIRST_FLOOR,
     ID_ROOM_LIVING_ROOM,
     ID_ROOM_KITCHEN,
@@ -62,6 +76,11 @@ module.exports.identifier = {
     ID_ROOM_GUESTROOM,
     ID_ROOM_SECOND_FLOOR,
     ID_ROOM_BATHROOM
+}
+module.exports.lightIds = {
+    ID_LIGHT_MAIN,
+    ID_LIGHT_SMALL_1,
+    ID_LIGHT_SMALL_2
 }
 // exports rooms
 module.exports.rooms = {}
