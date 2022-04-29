dadf721851fc919fc907b485e5a4eb389ac77c90
Marcel Stolin <mstolin@mailbox.org>
2022-04-29 16:53:45 +0200

    Udpated the goal and added a test suite

--- better-sleep/src/lib/bdi/Goal.js
+++ better-sleep/src/lib/bdi/Goal.js
@@ -1,13 +1,21 @@
 class Goal {
 
+    /** @type {string} */
+    #predicate
+    /** @type {any} */
     #condition
 
     /**
      * 
-     * @param {any} condition
+     * @param {object} condition
      */
-    constructor(condition) {
+    constructor(predicate, condition) {
+        this.#predicate = predicate
         this.#condition = condition
+    }
+
+    get predicate() {
+        return this.#predicate
     }
 
     /**
@@ -15,19 +23,7 @@ class Goal {
      */
     get condition() {
         return this.#condition
-    }
-
-    /**
-     * Checks whether the given conditions have been filfilled.
-     * This has to be implemented for each Goal individually.
-     * 
-     * @param {object} helper
-     * @returns {boolean} If the the condition is true
-     */
-    isConditionTrue(helper) {
-        // Return false on default, this takes action if this method is not being overwritten
-        return false
-    }
+    } 
 
 }
 
