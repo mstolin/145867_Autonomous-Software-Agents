4e09a2f45ab4a89a4ef6ab89a0c7555de10cc198
Marcel Stolin <mstolin@mailbox.org>
2022-04-29 19:01:46 +0200

    Added a first agent with intention beliefs and a goal

--- better-sleep/src/scenario/scenario.js
+++ better-sleep/src/scenario/scenario.js
@@ -1,30 +1,65 @@
-// Imports
-//const Agent = require('./lib/Agent')
+// BDI
+const Agent = require('../lib/bdi/Agent')
+const BeliefSet = require('../lib/bdi/BeliefSet')
+const Goal = require('../lib/bdi/Goal')
+const Intention = require('../lib/bdi/Intention')
+
+// Utils
 const Clock = require('../lib/utils/Clock')
-//const Goal = require('./lib/Goal')
+
+// World
 const House = require('../lib/House')
-//const Intention = require('./lib/Intention')
 const persons = require('../world/persons/Persons')
 const personIds = require('../world/persons/PersonIds')
 const rooms = require('../world/rooms/Rooms')
 const roomIds = require('../world/rooms/RoomIds')
+
+// Scenario
 const startRoutine = require('./Routine')
 
-// Create house
+// Create the house
 let defaultLocations = {}
 defaultLocations[personIds.ID_PERSON_SANDRA] = roomIds.ID_ROOM_BEDROOM
 defaultLocations[personIds.ID_PERSON_BOB] = roomIds.ID_ROOM_BEDROOM
-
 let house = new House(persons, rooms, defaultLocations)
 
-// Set observers
+// Set some test observers
 house.peopleLocations.observe(personIds.ID_PERSON_SANDRA, (v, k) => console.log(`${k} has entered ${v}`))
 house.peopleLocations.observe(personIds.ID_PERSON_BOB, (v, k) => console.log(`${k} has entered ${v}`))
 
-Clock.global.observe('mm', (key, mm) => {
+// Goals
+let morningLightGoal = new Goal('bedroom_light', 'on')
+class TurnOnLightInTheMorning extends Intention {
+    static applicable(goal) {
+        return goal instanceof MorningLightGoal
+    }
+    *exec() {
+        while(true) {
+            yield Clock.global.notifyChange('mm')
+            if(Clock.global.hh == 1 && Clock.global.mm == 0) {
+                console.log('Its 7, wake up!')
+                break
+            }
+        }
+    }
+}
+/*
+let goal = new Goal('some_light', 'on', (time) => { time.hh == 7 && time.mm == 0 })
+*/
+
+// House Agent
+let houseAgentBeliefs = new BeliefSet(defaultLocations) // the house agent needs to know about the people locations
+let houseAgent = new Agent('House Agent', houseAgentBeliefs)
+houseAgent.addIntention(TurnOnLightInTheMorning)
+houseAgent.postSubGoal(morningLightGoal).catch(err => console.error(err))
+
+
+// Start the routine when the clock starts
+Clock.global.observe('mm', () => {
     let time = Clock.global
     startRoutine(time, house)
 })
 
+// Start the clock
 Clock.startTimer()
 Clock.wallClock()
