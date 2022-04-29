59de3fb8c9b5975ddd18bc421c0f77014f0c98d8
Marcel Stolin <mstolin@mailbox.org>
2022-04-29 16:52:20 +0200

    Updated the agent implementation and added tests

--- better-sleep/src/lib/bdi/Agent.js
+++ better-sleep/src/lib/bdi/Agent.js
@@ -1,11 +1,39 @@
-class HouseAgent {
+/**
+ * An Agent is based on Beliefs, Intentions and Goals.
+ * It uses its Intention to achieve a specific Goal,
+ * based on its Beliefs.
+ */
+class Agent {
 
+    /** @type {[Intention]} */
     #intentions = []
-
-    name
+    /** @type {string} */
+    #name
+    /** @type {BeliefSet} */
+    #beliefSet
     
-    constructor(name) {
-        this.name = name
+    /**
+     * 
+     * @param {string} name The agents name
+     * @param {BeliefSet} beliefSet The agents initial beliefs
+     */
+    constructor(name, beliefSet) {
+        this.#name = name
+        this.#beliefSet = beliefSet
+    }
+
+    /** The agents name */
+    get name() {
+        return this.#name
+    }
+
+    /** The agents beliefs */
+    get beliefs() {
+        return this.#beliefSet
+    }
+
+    updateBelief(name, value) {
+        this.#beliefSet.setPredicate(name, value)
     }
 
     /**
@@ -19,27 +47,39 @@ class HouseAgent {
     }
 
     /**
+     * Tries to achieve the given goal.
      * 
      * @param {Goal} subGoal 
      * @returns {Promise}
      */
     async postSubGoal(subGoal) {
+        // 1. Check if the agent does know about the predicate
+        if(!this.#beliefSet.includesPredicate(subGoal.predicate)) {
+            return Promise.reject(`${subGoal.predicate} is not part of the agent belief set`)
+        }
+
+        // 2. Check if any intention of this agent can be used to reach the given goal
         for(let IntentionClass of Object.values(this.#intentions)) {
             if(!IntentionClass.applicable(subGoal)) {
+                // Intention cannot be used to achieve the goal
                 continue
             }
 
             // Use this intention to reach the given subgoal
             let intention = new IntentionClass(subGoal)
+            // Run the plan of the intention to reach the goal
             let success = await intention
                 .run()
                 .catch(err => {
-                    console.log('Error', err)
+                    console.log(`Error running intention ${IntentionClass}`, err)
                 })
 
             if(success) {
+                // Plan was successful
                 return Promise.resolve(true)
             } else {
+                // Plan was not successful to reach the goal
+                // Try the next intention
                 continue
             }
         }
@@ -49,4 +89,4 @@ class HouseAgent {
 
 }
 
-module.exports = HouseAgent
+module.exports = Agent
---
+++ better-sleep/src/lib/bdi/Agent.test.js
@@ -0,0 +1,37 @@
+const Agent = require('./Agent')
+const BeliefSet = require('./BeliefSet')
+const Intention = require('./Intention')
+const Goal = require('./Goal')
+
+test('Agent.constructor', () => {
+    let beliefs = new BeliefSet({
+        'kitchen_light': 'off'
+    })
+    let agent = new Agent('007', beliefs)
+    expect(agent.name).toBe('007')
+    expect(agent.beliefs).toBe(beliefs)
+})
+
+test('Agent.postSubGoal', async() => {
+    var kitchenLightState = 'off'
+
+    let beliefs = new BeliefSet({
+        'kitchen_light': 'off'
+    })
+    // New agent that beliefs the kitchen light is off
+    let agent = new Agent('007', beliefs)
+
+    // A new goal to turn on the kitchen light
+    let kitchenLightOn = new Goal('kitchen_light', 'on')
+
+    // This intention is supposed to turn the kitchen light on if its off
+    class TurnLightOn extends Intention {
+       *exec() {
+           kitchenLightState = yield 'on'
+       } 
+    }
+    agent.addIntention(TurnLightOn)
+
+    expect(await agent.postSubGoal(kitchenLightOn)).toBe(true)
+    expect(kitchenLightState).toBe('on')
+})
