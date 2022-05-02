175cd296778b4b00355722cc3b8a9e10f3ef74cc
Marcel Stolin <mstolin@mailbox.org>
2022-05-02 12:46:54 +0200

    Open all shutters in the morning

--- better-sleep/src/scenario/scenario.js
+++ better-sleep/src/scenario/scenario.js
@@ -22,38 +22,63 @@ const startRoutine = require('./Routine')
     ENVIRONMENT
 */
 // Set some test observers
-house.getPerson(personIds.ID_PERSON_SANDRA).observe('location', (v, _) => console.log(`Sandra has entered ${v}`))
-house.getPerson(personIds.ID_PERSON_BOB).observe('location', (v, _) => console.log(`Bob has entered ${v}`))
-house.getRoom(roomIds.ID_ROOM_BEDROOM).mainLight.observe('state', (v, _) => console.log(`Bedroom main light changed state to ${v}`))
+house.getPerson(personIds.ID_PERSON_SANDRA).observe('location', (v, _) => console.log(`[SANDRA] Sandra has entered ${v}`))
+house.getPerson(personIds.ID_PERSON_BOB).observe('location', (v, _) => console.log(`[BOB] Bob has entered ${v}`))
+house.getRoom(roomIds.ID_ROOM_BEDROOM).mainLight.observe('state', (v, _) => console.log(`[BEDROOM] Bedroom main light changed state to ${v}`))
+Object
+    .keys(house.rooms)
+    .forEach(room => house.getRoom(room).shutters.forEach(
+        shutter => shutter.observe('state', (v, _) => console.log(`[${room}] Shutter has changed state to ${v}`))
+    )
+)
 
 /*
     AGENTS, INTENTIONS, AND GOALS
 */
-// Goals
+// Turn on the light in the morning
 let morningLightGoal = new Goal('bedroom_light', 'on')
 class TurnOnLightInTheMorning extends Intention {
-    /*static applicable(goal) {
-        return goal instanceof MorningLightGoal
-    }*/
+    static applicable(goal) {
+        return goal == 'morningLight'
+    }
     *exec() {
-        while(true) {
-            yield Clock.global.notifyChange('mm')
-            if(Clock.global.hh == 1 && Clock.global.mm == 0) {
-                console.log('Its 7, wake up!')
+        Clock.global.observe('mm', () => {
+            let time = Clock.global
+            if(time.hh == 1 && time.mm == 0) {
                 house.getRoom(roomIds.ID_ROOM_BEDROOM).mainLight.turnOn(500, 2000)
-                break
             }
-        }
+        })
+    }
+}
+
+// Open all shutters in the morning
+let shuttersOpenGoal = new Goal('shutters', 'open')
+class OpenShutters extends Intention {
+    static applicable(goal) {
+        return goal == 'openShutters'
+    }
+    *exec() {
+        Clock.global.observe('mm', () => {
+            let time = Clock.global
+            if(time.hh == 1 && time.mm == 0) {
+                // Open all shutters
+                Object.keys(house.rooms)
+                    .forEach(room => house.getRoom(room).shutters.forEach(shutter => shutter.open()))
+            }
+        })
     }
 }
 
 // House Agent
-let beliefs = _.merge(house.locations, {'bedroom_light': 'off'})
+let beliefs = _.merge(house.locations, {'bedroom_light': 'off', 'shutters': 'closed'})
 let houseAgentBeliefs = new BeliefSet(beliefs) // the house agent needs to know about the people locations
 let houseAgent = new Agent('House Agent', houseAgentBeliefs)
 houseAgent.addIntention(TurnOnLightInTheMorning)
-houseAgent.postSubGoal(morningLightGoal).catch(err => console.error('Not able to achieve goal morningLightGoal:', err))
-
+houseAgent.addIntention(OpenShutters)
+houseAgent.postSubGoals({
+    'morningLight': morningLightGoal,
+    'openShutters': shuttersOpenGoal
+}).catch(err => console.log(err))
 
 /*
     ROUTINE
@@ -66,4 +91,4 @@ Clock.global.observe('mm', () => {
 
 // Start the clock
 Clock.startTimer()
-Clock.wallClock()
+//Clock.wallClock()
