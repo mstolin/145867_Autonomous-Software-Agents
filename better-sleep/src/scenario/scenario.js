a1d45f6794f625363e9c749441ab8b214a2011d8
Marcel Stolin <mstolin@mailbox.org>
2022-05-02 14:44:52 +0200

    Used the single bdi modules now in the scenario

--- better-sleep/src/scenario/scenario.js
+++ better-sleep/src/scenario/scenario.js
@@ -4,10 +4,8 @@
 // libs
 const _ = require('lodash');
 // BDI
-const Agent = require('../lib/bdi/Agent')
-const BeliefSet = require('../lib/bdi/BeliefSet')
-const Goal = require('../lib/bdi/Goal')
-const Intention = require('../lib/bdi/Intention')
+const houseAgent = require('./bdi/agents/house-agent/HouseAgent')
+const houseAgentGoals = require('./bdi/agents/house-agent/Goals')
 // Utils
 const Clock = require('../lib/utils/Clock')
 // World
@@ -35,50 +33,8 @@ Object
 /*
     AGENTS, INTENTIONS, AND GOALS
 */
-// Turn on the light in the morning
-let morningLightGoal = new Goal('bedroom_light', 'on')
-class TurnOnLightInTheMorning extends Intention {
-    static applicable(goal) {
-        return goal == 'morningLight'
-    }
-    *exec() {
-        Clock.global.observe('mm', () => {
-            let time = Clock.global
-            if(time.hh == 1 && time.mm == 0) {
-                house.getRoom(roomIds.ID_ROOM_BEDROOM).mainLight.turnOn(500, 2000)
-            }
-        })
-    }
-}
+houseAgent.postSubGoals(houseAgentGoals).catch(err => console.log(err))
 
-// Open all shutters in the morning
-let shuttersOpenGoal = new Goal('shutters', 'open')
-class OpenShutters extends Intention {
-    static applicable(goal) {
-        return goal == 'openShutters'
-    }
-    *exec() {
-        Clock.global.observe('mm', () => {
-            let time = Clock.global
-            if(time.hh == 1 && time.mm == 0) {
-                // Open all shutters
-                Object.keys(house.rooms)
-                    .forEach(room => house.getRoom(room).shutters.forEach(shutter => shutter.open()))
-            }
-        })
-    }
-}
-
-// House Agent
-let beliefs = _.merge(house.locations, {'bedroom_light': 'off', 'shutters': 'closed'})
-let houseAgentBeliefs = new BeliefSet(beliefs) // the house agent needs to know about the people locations
-let houseAgent = new Agent('House Agent', houseAgentBeliefs)
-houseAgent.addIntention(TurnOnLightInTheMorning)
-houseAgent.addIntention(OpenShutters)
-houseAgent.postSubGoals({
-    'morningLight': morningLightGoal,
-    'openShutters': shuttersOpenGoal
-}).catch(err => console.log(err))
 
 /*
     ROUTINE
