af992f972c1f07b153cffb8f94d6ac8d3f87e87e
Marcel Stolin <mstolin@mailbox.org>
2022-05-02 15:10:25 +0200

    Updated scenario to use the new Logger

--- better-sleep/src/scenario/Scenario.js
+++ better-sleep/src/scenario/Scenario.js
@@ -8,6 +8,7 @@ const houseAgent = require('./bdi/agents/house-agent/HouseAgent')
 const houseAgentGoals = require('./bdi/agents/house-agent/Goals')
 // Utils
 const Clock = require('../lib/utils/Clock')
+const Logger = require('../lib/utils/Logger')
 // World
 const house = require('./world/House')
 const personIds = require('./world/persons/PersonIds')
@@ -20,13 +21,13 @@ const startRoutine = require('./Routine')
     ENVIRONMENT
 */
 // Set some test observers
-house.getPerson(personIds.ID_PERSON_SANDRA).observe('location', (v, _) => console.log(`[SANDRA] Sandra has entered ${v}`))
-house.getPerson(personIds.ID_PERSON_BOB).observe('location', (v, _) => console.log(`[BOB] Bob has entered ${v}`))
-house.getRoom(roomIds.ID_ROOM_BEDROOM).mainLight.observe('state', (v, _) => console.log(`[BEDROOM] Bedroom main light changed state to ${v}`))
+house.getPerson(personIds.ID_PERSON_SANDRA).observe('location', (v, _) => Logger.prefix(personIds.ID_PERSON_SANDRA).log(`Has entered ${v}`))
+house.getPerson(personIds.ID_PERSON_BOB).observe('location', (v, _) => Logger.prefix(personIds.ID_PERSON_BOB).log(`Has entered ${v}`))
+house.getRoom(roomIds.ID_ROOM_BEDROOM).mainLight.observe('state', (v, _) => Logger.prefix(roomIds.ID_ROOM_BEDROOM).log(`Main light has changed state to ${v}`))
 Object
     .keys(house.rooms)
     .forEach(room => house.getRoom(room).shutters.forEach(
-        shutter => shutter.observe('state', (v, _) => console.log(`[${room}] Shutter has changed state to ${v}`))
+        shutter => shutter.observe('state', (v, _) => Logger.prefix(room).log(`Shutter has changed state to ${v}`))
     )
 )
 
