26c722348d54991f4ff91aed9e8ebcea53077ccb
Marcel Stolin <mstolin@mailbox.org>
2022-05-18 12:09:09 +0200

    Updated design of the agent implementation

--- better-sleep/src/lib/bdi/Agent4.js
+++ better-sleep/src/lib/bdi/Agent4.js
@@ -1,30 +1,53 @@
 const Beliefset =  require('./BeliefSet2')
 const Intention = require('./Intention2');
 
-
-
 /**
  * @class Agent
  */
 class Agent {
 
+    /** @type {Array<Intention>} intentions */
+    #intentions = []
+    /** @type {Beliefset} beliefs */
+    #beliefs = new Beliefset()
+    /** @type {String} name */
+    #name
+    
+    /**
+     * Initializes an Agent.
+     * 
+     * @param {String} name 
+     */
     constructor (name) {
-        this.name = name
-
-        /** @type {Beliefset} beliefs */
-        this.beliefs = new Beliefset()
-
-        this.beliefs.observeAny( (v,fact) => this.log('Belief changed: ' + (v?fact:'not '+fact) ) )
-
-        /** @type {Array<Intention>} intentions */
-        this.intentions = []
+        this.#name = name
+        this.#beliefs.observeAny((v, fact) => this.log('Belief changed: ' + (v?fact:'not '+fact)))
     }
 
-    log (...args) {
-        console.log( this.name + '\t\t', ...args )
+    /**
+     * Returns the agents name.
+     */
+    get name() {
+        return this.#name
     }
 
-    async postSubGoal (subGoal) {
+    /**
+     * Logs a message on behalf of the agent.
+     * @param  {...any} args 
+     */
+    log(...args) {
+        console.log(this.name + '\t\t', ...args)
+    }
+
+    /**
+     * Adds an intention to the agents.
+     * 
+     * @param {Intention} intention 
+     */
+    addIntention(intention) {
+        this.#intentions.push(intention)
+    }
+
+    async postSubGoal(subGoal) {
         
         // if (!this.beliefs.check(subGoal.precondition)) { //!subGoal.checkPrecondition()
         //     this.log('Goal cannot be taken, preconditions are not valid', subGoal)
@@ -33,22 +56,20 @@ class Agent {
         //     return
         // }
         
-        for (let intentionClass of Object.values(this.intentions)) {
-            
-            if (!intentionClass.applicable(subGoal)) // By default applicable(goal) returns true (see class Intention)
-                continue; // if not applicable try next
+        for(const intentionClass of Object.values(this.#intentions)) {
+            if (!intentionClass.applicable(subGoal)) { // By default applicable(goal) returns true (see class Intention)
+                continue // if not applicable try next
+            }
     
             this.log('Trying to use intention', intentionClass.name, 'to achieve goal', subGoal.toString())
     
             var intention = new intentionClass(this, subGoal)
-            
             var success = await intention.run().catch( err => {this.log('Error in run() intention:', err)} )
     
-            if ( success ) {
+            if(success) {
                 this.log('Succesfully used intention', intentionClass.name, 'to achieve goal', subGoal.toString())
                 return Promise.resolve(true) // same as: return true;
-            }
-            else {
+            } else {
                 this.log('Failed to use intention', intentionClass.name, 'to achieve goal', subGoal.toString())
                 continue // retrying
             }
