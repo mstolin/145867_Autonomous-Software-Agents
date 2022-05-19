007b36a1f83f51f87f60a267dfb8367f92219c43
Marcel Stolin <mstolin@mailbox.org>
2022-05-19 13:29:57 +0200

    Updated light sensor to try to calc a fake value

--- better-sleep/src/lib/world/LightSensor.js
+++ better-sleep/src/lib/world/LightSensor.js
@@ -1,20 +1,37 @@
 const Device = require("./Device");
 
 class LightSensor extends Device {
-    constructor(name, light) {
-        super(name);
+    #room;
 
-        this.set(
-            "roomIlluminence",
-            this.#calculateRoomIlluminence(light.brightness)
+    constructor(name, room) {
+        super(name);
+        this.#room = room;
+
+        // calculate room illuminence on init
+        this.set("roomIlluminence", this.#calculateRoomIlluminence());
+
+        // observe the main light
+        this.#room.mainLight.observe("brightness", (_) =>
+            this.#updateRoomIlluminence()
         );
-        light.observe("brightness", (brightness) =>
-            this.#calculateRoomIlluminence(brightness)
+        // observe the shutters
+        this.#room.shutters.forEach((shutter) =>
+            shutter.observe("state", (_) => this.#updateRoomIlluminence())
         );
     }
 
-    #calculateRoomIlluminence(brightness) {
-        return 5;
+    #updateRoomIlluminence() {
+        let illuminence = this.#calculateRoomIlluminence();
+        this.set("roomIlluminence", illuminence);
+    }
+
+    #calculateRoomIlluminence() {
+        let mainLightbrightness = this.#room.mainLight.brightness;
+        let openShutters = this.#room.shutters.filter(
+            (shutter) => shutter.isOpen
+        ).length;
+        let shutterBrightness = openShutters * 500;
+        return mainLightbrightness + shutterBrightness;
     }
 }
 
