4b95c69d41589869a2de1a78972ecd6f37cef3e4
Marcel Stolin <mstolin@mailbox.org>
2022-05-20 18:37:05 +0200

    Created RoomAgent and use it

--- better-sleep/src/scenario/bdi/agents/room-agent/index.js
+++ better-sleep/src/scenario/bdi/agents/room-agent/index.js
@@ -1,14 +1,13 @@
-const Agent = require("../../../../lib/bdi/Agent");
+const RoomAgent = require("../../../../lib/bdi/RoomAgent");
 const intentions = require("./Intentions");
 const roomIds = require("../../../world/rooms/RoomIds");
 const rooms = require("../../../world/rooms");
 
 const initRoomAgent = (id, room) => { // TODO MAKE Custom class RoomAgent with room param
-    let agent = new Agent(`Room-Agent-${id}`);
+    let agent = new RoomAgent(`Room-Agent-${id}`, room);
     for (const intention of intentions) {
         agent.intentions.push(intention);
     }
-    agent.room = room;
     return agent;
 };
 
