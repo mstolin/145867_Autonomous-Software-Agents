ea369bce394dc98a808667a3224d7539896b5283
Marcel Stolin <mstolin@mailbox.org>
2022-05-20 10:24:57 +0200

    Formatted person class

--- better-sleep/src/lib/world/Person.js
+++ better-sleep/src/lib/world/Person.js
@@ -1,46 +1,44 @@
-const Observable = require('../utils/Observable')
+const Observable = require("../utils/Observable");
 
 class Person extends Observable {
-
     /** @type {string} */
-    #name
+    #name;
 
     /**
      * Constructs a Person instance.
-     * 
+     *
      * @param {string} name Name of the Person
      */
     constructor(name) {
-        super({})
-        this.#name = name
+        super({});
+        this.#name = name;
     }
 
     get name() {
-        return this.#name
+        return this.#name;
     }
 
     /**
      * Updates the persons location.
-     * 
+     *
      * @param {string} roomId ID of the desired room
      */
     setLocation(roomId) {
-        if(this.isInRoom(roomId)) {
-            throw(`${this.#name} is already in ${roomId}`)
+        if (this.isInRoom(roomId)) {
+            throw `${this.#name} is already in ${roomId}`;
         }
-        this.set('location', roomId)
+        this.set("location", roomId);
     }
 
     /**
      * Checks if the person is in a room.
-     * 
+     *
      * @param {string} roomId ID of some room
      * @returns {boolean} True if person is in the given room
      */
     isInRoom(roomId) {
-        return roomId == this.location
+        return roomId == this.location;
     }
-
 }
 
-module.exports = Person
+module.exports = Person;
