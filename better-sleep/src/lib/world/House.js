10bcf247d9a9f57e0930a0da196870281207ff0e
Marcel Stolin <mstolin@mailbox.org>
2022-05-22 17:11:51 +0200

    Updated occupation status after movement

--- better-sleep/src/lib/world/House.js
+++ better-sleep/src/lib/world/House.js
@@ -1,79 +1,82 @@
 class House {
-
     /** @type {object} */
-    #people
+    #people;
     /** @type {object} */
-    #rooms
+    #rooms;
 
     /**
      * Constructs a new House instance.
-     * 
-     * @param {object} people The people of this house 
-     * @param {object} rooms All rooms of the house 
+     *
+     * @param {object} people The people of this house
+     * @param {object} rooms All rooms of the house
      */
     constructor(people, rooms) {
-        this.#people = people
-        this.#rooms = rooms
+        this.#people = people;
+        this.#rooms = rooms;
     }
 
     get people() {
-        return this.#people
+        return this.#people;
     }
 
     get rooms() {
-        return this.#rooms
+        return this.#rooms;
     }
 
     /**
      * Returns the room for the given ID.
-     * 
+     *
      * @param {string} roomId Desired room
-     * @returns 
+     * @returns
      */
     getRoom(roomId) {
-        if(!this.hasRoom(roomId)) {
-            throw(`Room with ID ${roomId} does not exist`)
+        if (!this.hasRoom(roomId)) {
+            throw `Room with ID ${roomId} does not exist`;
         }
-        return this.#rooms[roomId]
+        return this.#rooms[roomId];
     }
 
     getPerson(personId) {
-        if(!this.hasPerson(personId)) {
-            throw(`Person with ID ${personId} does not exist`)
+        if (!this.hasPerson(personId)) {
+            throw `Person with ID ${personId} does not exist`;
         }
-        return this.#people[personId]
+        return this.#people[personId];
     }
 
     hasRoom(roomId) {
-        return this.#rooms.hasOwnProperty(roomId)
+        return this.#rooms.hasOwnProperty(roomId);
     }
 
     hasPerson(personId) {
-        return this.#people.hasOwnProperty(personId)
+        return this.#people.hasOwnProperty(personId);
     }
 
     /**
      * Moves a person from one room to another.
      * It is important that the start room has a direct path
      * to the destination.
-     * 
+     *
      * @param {string} person The person identifier
      * @param {string} from Identifer of the source room
      * @param {string} to Identifier of the destination room
      */
     movePersonTo(personId, sourceId, destinationId) {
-        let person = this.getPerson(personId)
-        let sourceRoom = this.getRoom(sourceId)
-        let destinationRoom = this.getRoom(destinationId)
-        if(!sourceRoom.hasPathToRoom(destinationId) && !destinationRoom.hasPathToRoom(sourceId)) {
-            throw(`There is no direct path from ${sourceId} to ${destinationId}.`)
+        let person = this.getPerson(personId);
+        let sourceRoom = this.getRoom(sourceId);
+        let destinationRoom = this.getRoom(destinationId);
+        if (
+            !sourceRoom.hasPathToRoom(destinationId) &&
+            !destinationRoom.hasPathToRoom(sourceId)
+        ) {
+            throw `There is no direct path from ${sourceId} to ${destinationId}.`;
         }
-        if(!person.isInRoom(sourceId)) {
-            throw(`Person ${personId} is not in ${sourceId}`)
+        if (!person.isInRoom(sourceId)) {
+            throw `Person ${personId} is not in ${sourceId}`;
         }
-        person.setLocation(destinationId)
+        person.setLocation(destinationId);
+        sourceRoom.removeResident(personId);
+        destinationRoom.addResident(personId);
     }
-    
 }
 
-module.exports = House
+module.exports = House;
