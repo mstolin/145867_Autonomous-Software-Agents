7144d2c2188c5feda27b43aba910a33c0fbde2be
Marcel Stolin <mstolin@mailbox.org>
2022-05-24 14:54:27 +0200

    Added empty SenseIlluminanceIntention

---
+++ better-sleep/src/scenario/bdi/agents/house-agent/intentions/SenseIlluminanceIntention.js
@@ -0,0 +1,5 @@
+// TODO IlluminanceIntention
+// Ideally we have a Sun/Moon according to the daytime
+// We calculate the illuminance of the room according to the light brightness,
+// and the outside brigtness.
+// Then we adjust the room light brightness accordingly.
