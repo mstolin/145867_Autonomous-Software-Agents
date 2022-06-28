6bd94517e74b3596f101caa39e4702f0c065a9e9
Marcel Stolin <mstolin@mailbox.org>
2022-06-28 18:03:15 +0200

    Renamed House-Agent

--- better-sleep/src/scenario/bdi/agents/house-agent/HouseAgent.js
+++ better-sleep/src/scenario/bdi/agents/house-agent/HouseAgent.js
@@ -2,7 +2,7 @@ const Agent = require("../../../../lib/bdi/Agent");
 const intentions = require("./intentions");
 const beliefs = require("./Beliefs");
 
-let houseAgent = new Agent("House Agent", beliefs);
+let houseAgent = new Agent("House-Agent", beliefs);
 // add all intentions
 for (let intention of intentions) {
     houseAgent.intentions.push(intention);
