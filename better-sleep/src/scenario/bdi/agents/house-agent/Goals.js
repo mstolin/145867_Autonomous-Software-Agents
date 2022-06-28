28e58741c3b57c3bf324f3d80fa35f0544a6b3b3
Marcel Stolin <mstolin@mailbox.org>
2022-06-28 17:56:31 +0200

    Removed Morning and EveningIntention of HouseAgent this is part of the corresponding agent

--- better-sleep/src/scenario/bdi/agents/house-agent/Goals.js
+++ better-sleep/src/scenario/bdi/agents/house-agent/Goals.js
@@ -8,9 +8,12 @@ class SenseMovementGoal extends Goal {}
 
 class SenseDaytimeGoal extends Goal {}
 
+class SenseIlluminanceGoal extends Goal {}
+
 module.exports = {
-    MorningGoal,
-    EveningGoal,
-    SenseMovementGoal,
-    SenseDaytimeGoal,
+  MorningGoal,
+  EveningGoal,
+  SenseMovementGoal,
+  SenseDaytimeGoal,
+  SenseIlluminanceGoal,
 };
