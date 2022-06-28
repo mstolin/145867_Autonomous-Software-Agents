719ac61b564573de4e78e9d7653060a7b25d7d6c
Marcel Stolin <mstolin@mailbox.org>
2022-06-28 17:58:07 +0200

    Removed morning and evening intentions from house agent intentions

--- better-sleep/src/scenario/bdi/agents/house-agent/intentions/index.js
+++ better-sleep/src/scenario/bdi/agents/house-agent/intentions/index.js
@@ -1,11 +1,7 @@
-const MorningIntention = require("./MorningIntention");
-const EveningIntention = require("./EveningIntention");
 const SenseMovementIntention = require("./SenseMovementIntention");
 const SenseDaytimeIntention = require("./SenseDaytimeIntention");
 
 module.exports = [
-    MorningIntention,
-    EveningIntention,
     SenseMovementIntention,
     SenseDaytimeIntention,
 ];
