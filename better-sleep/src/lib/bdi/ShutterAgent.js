650e42291822e7613705f882d24e61501a2ccfd7
Marcel Stolin <mstolin@mailbox.org>
2022-06-28 17:56:59 +0200

    Created a shutter agent class

---
+++ better-sleep/src/lib/bdi/ShutterAgent.js
@@ -0,0 +1,28 @@
+const Agent = require("./Agent");
+
+class ShutterAgent extends Agent {
+
+    /** @type {Room} */
+    #room;
+
+    /**
+     * Constructs a RoomAgent instance.
+     *  
+     * @param {string} name 
+     * @param {Room} room 
+     */
+    constructor(name, room) {
+        super(name);
+        this.#room = room;
+    }
+
+    /**
+     * Returns the agents room.
+     */
+    get room() {
+        return this.#room;
+    }
+
+}
+
+module.exports = ShutterAgent;
