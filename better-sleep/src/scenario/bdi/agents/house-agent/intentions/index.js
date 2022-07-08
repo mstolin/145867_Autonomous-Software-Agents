bad3550920812ae84462ab88c5044e1242f2d129
Marcel Stolin <mstolin@mailbox.org>
2022-07-08 14:00:13 +0200

    Added senseIlluminanceIntention to house agents intentions

--- better-sleep/src/scenario/bdi/agents/house-agent/intentions/index.js
+++ better-sleep/src/scenario/bdi/agents/house-agent/intentions/index.js
@@ -1,7 +1,9 @@
 const SenseMovementIntention = require("./SenseMovementIntention");
 const SenseDaytimeIntention = require("./SenseDaytimeIntention");
+const SenseIlluminanceIntention = require("./SenseIlluminanceIntention");
 
 module.exports = [
     SenseMovementIntention,
     SenseDaytimeIntention,
+    SenseIlluminanceIntention,
 ];
