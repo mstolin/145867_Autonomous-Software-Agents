33bafdfdac24efafdf2d188e754f3dbd1b723b17
Marcel Stolin <mstolin@mailbox.org>
2022-07-12 10:25:45 +0200

    Updated sensor to calculate random illuminance

--- better-sleep/src/lib/world/IlluminanceSensor.js
+++ better-sleep/src/lib/world/IlluminanceSensor.js
@@ -1,34 +1,41 @@
 const Device = require("./Device");
-const Clock = require("../utils/Clock")
+const Clock = require("../utils/Clock");
 
 class IlluminanceSensor extends Device {
-    #room;
 
-    constructor(name, room) {
-        super(name);
-        this.#room = room;
+  constructor(name) {
+    super(name);
 
-        // calculate room illuminence on init
-        this.set("illuminence", this.#determineOutdoorIlluminence(Clock.global["hh"]));
-        Clock.global.observe("hh", (hour) => {
-            this.set("illuminence", this.#determineOutdoorIlluminence(hour));
-        });
+    // calculate room illuminence on init
+    this.set(
+      "illuminence",
+      this.#determineOutdoorIlluminence(Clock.global["hh"])
+    );
+    Clock.global.observe("hh", (hour) => {
+      this.set("illuminence", this.#determineOutdoorIlluminence(hour));
+    });
+  }
+
+  #isMorning = (hour) => hour >= 6 && hour < 12;
+
+  #isAfternoon = (hour) => hour >= 12 && hour < 18;
+
+  #determineOutdoorIlluminence(hour) {
+    // These are totally random number to simulate illuminance change
+    if (this.#isMorning(hour)) {
+      return this.#getRandomArbitrary(0.25, 1);
+    } else if (this.#isAfternoon(hour)) {
+      return this.#getRandomArbitrary(0.5, 1);;
+    } else {
+      // evening
+      return this.#getRandomArbitrary(0.1, 0.3);;
     }
+  }
 
-    #isMorning = (hour) => hour >=6 && hour < 12;
-
-    #isAfternoon = (hour) => hour >= 12 && hour < 18;
-
-    #determineOutdoorIlluminence(hour) {
-        if (this.#isMorning(hour)) {
-            return 1;
-        } else if (this.#isAfternoon(hour)) {
-            return 2;
-        } else {
-            // evening
-            return 3;
-        }
-    }
+  #getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
 }
 
 module.exports = IlluminanceSensor;
