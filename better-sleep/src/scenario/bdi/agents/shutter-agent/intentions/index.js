2fdd5fd8fb62c47f8c40269cfb603c6b2094ea97
Marcel Stolin <mstolin@mailbox.org>
2022-07-12 10:26:26 +0200

    Fixed shutter agent intentions to only use morning and evening intention

--- better-sleep/src/scenario/bdi/agents/shutter-agent/intentions/index.js
+++ better-sleep/src/scenario/bdi/agents/shutter-agent/intentions/index.js
@@ -1,6 +1,12 @@
-const OpenShutterIntention = require("./OpenAllShuttersIntention");
-const CloseShuttersIntention = require("./CloseShuttersIntention");
-const OpenAllShuttersMorningIntention = require("./OpenAllShuttersMorningIntention");
+const OpenAllShuttersIntention = require("./OpenAllShuttersIntention");
+const OpenAllShuttersHalfwayIntention = require("./OpenAllShuttersHalfwayIntention");
+const CloseShuttersIntention = require("./CloseShuttersIntention"); // TODO Still needed?
+const OpenAllShuttersMorningIntention = require("./OpenAllShuttersMorningIntention"); // TODO Still needed?
 const CloseAllShuttersEveningIntention = require("./CloseAllShuttersEveningIntention");
 
-module.exports = [OpenAllShuttersMorningIntention, CloseAllShuttersEveningIntention];
+module.exports = [
+    //OpenAllShuttersIntention,
+    //OpenAllShuttersHalfwayIntention,
+    OpenAllShuttersMorningIntention,
+    CloseAllShuttersEveningIntention,
+];
