41ca7c04f8ca85dce9bc4c0bf6f6fd7f887b7b9c
Marcel Stolin <mstolin@mailbox.org>
2022-07-19 13:42:50 +0200

    Formatted document and added more comments

--- better-sleep/src/scenario/Scenario.js
+++ better-sleep/src/scenario/Scenario.js
@@ -8,26 +8,29 @@ const roomIds = require("./world/rooms/RoomIds");
 // BDI
 const houseAgent = require("./bdi/agents/house-agent/HouseAgent");
 const shutterAgents = require("./bdi/agents/shutter-agent");
-const bedroomAgent = require("./bdi/agents/room-agent")[roomIds.ID_ROOM_BEDROOM];
+const bedroomAgent = require("./bdi/agents/room-agent")[
+    roomIds.ID_ROOM_BEDROOM
+];
 const {
     SenseMovementGoal,
     SenseDaytimeGoal,
-    SenseIlluminanceGoal
+    SenseIlluminanceGoal,
 } = require("./bdi/agents/house-agent/Goals");
 const {
     TurnOnShuttersGoal,
-    TurnOffShuttersGoal
+    TurnOffShuttersGoal,
 } = require("./bdi/agents/shutter-agent/Goals");
-const {
-    WakeUpGoal,
-    SleepGoal
-} = require("./bdi/agents/room-agent/Goals");
+const { WakeUpGoal, SleepGoal } = require("./bdi/agents/room-agent/Goals");
 // Utils
 const Clock = require("../lib/utils/Clock");
 // Scenario
 const startRoutine = require("./Routine");
 // Observers
-const { observeAllRooms, observeAllPersons, observeHouseSensors } = require("./observers");
+const {
+    observeAllRooms,
+    observeAllPersons,
+    observeHouseSensors,
+} = require("./observers");
 // Environment
 const { initEnvironment, turnOnSensors } = require("./Environment");
 
@@ -63,6 +66,9 @@ Clock.global.observe("mm", () => {
 });
 
 Clock.global.observe("dd", () => {
+    // Post goals in this observer, because these need to be recurring
+    // every day.
+
     /* Goal is to turn on light in the morning to wake up everyone in the bedroom
     and to turn in it off when they are going to sleep. To adjust brightness and
     temp, PDDL intentions are supposed to handle this.
@@ -74,7 +80,7 @@ Clock.global.observe("dd", () => {
         /* The goal is to tun on the shutters in morning, when everybody
         wakes up and to turn them off in the evening when they are going
         to sleep. We only need to turn the devices on or off. PDDL intentions
-        will open them according to the outdoor illuminance automaticcaly. 
+        will open them according to the outdoor illuminance automatically. 
         */
         shutterAgent.postSubGoal(new TurnOnShuttersGoal({ hh: 7, mm: 0 }));
         shutterAgent.postSubGoal(new TurnOffShuttersGoal({ hh: 23, mm: 0 }));
