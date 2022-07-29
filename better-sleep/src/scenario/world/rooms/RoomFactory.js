7a7deaf2398ffbdadb40b733a048639f72c5544b
Marcel Stolin <mstolin@mailbox.org>
2022-07-29 19:22:12 +0200

    Created a RoomFactory class

---
+++ better-sleep/src/scenario/world/rooms/RoomFactory.js
@@ -0,0 +1,27 @@
+const Room = require("../../../lib/world/Room");
+const roomAgent = require("../../bdi/agents/room-agent");
+const ids = require("./RoomIds");
+
+const doors = {};
+doors[ids.ID_ROOM_FIRST_FLOOR] = [ids.ID_ROOM_LIVING_ROOM, ids.ID_ROOM_KITCHEN];
+doors[ids.ID_ROOM_LIVING_ROOM] = [ids.ID_ROOM_KITCHEN, ids.ID_ROOM_FIRST_FLOOR];
+doors[ids.ID_ROOM_KITCHEN] = [ids.ID_ROOM_LIVING_ROOM, ids.ID_ROOM_FIRST_FLOOR];
+doors[ids.ID_ROOM_SECOND_FLOOR] = [
+    ids.ID_ROOM_BEDROOM,
+    ids.ID_ROOM_BATHROOM,
+    ids.ID_ROOM_GUESTROOM,
+    ids.ID_ROOM_FIRST_FLOOR,
+];
+doors[ids.ID_ROOM_BEDROOM] = [ids.ID_ROOM_SECOND_FLOOR];
+doors[ids.ID_ROOM_GUESTROOM] = [ids.ID_ROOM_SECOND_FLOOR];
+doors[ids.ID_ROOM_BATHROOM] = [ids.ID_ROOM_FIRST_FLOOR];
+
+class RoomFactory {
+    static #numberOfShutters = 2;
+
+    static createRoom(id) {
+        return new Room(id, doors[id], this.#numberOfShutters, roomAgent, null);
+    }
+}
+
+module.exports = RoomFactory;
