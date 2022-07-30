1f21a324467cd04d43554918ea7082246c4dcbfe
Marcel Stolin <mstolin@mailbox.org>
2022-07-30 12:00:27 +0200

    Removed single person files and created initmethod

--- better-sleep/src/scenario/world/persons/Bob.js
+++
@@ -1,3 +0,0 @@
-const Person = require('../../../lib/world/Person')
-
-module.exports = new Person('Bob')
--- better-sleep/src/scenario/world/persons/Persons.js
+++ better-sleep/src/scenario/world/persons/Persons.js
@@ -1,9 +1,14 @@
-const ids = require('./PersonIds')
-const Sandra = require('./Sandra')
-const Bob = require('./Bob')
+const Person = require("../../../lib/world/Person");
+const ids = require("./PersonIds");
 
-let persons = {}
-persons[ids.ID_PERSON_SANDRA] = Sandra
-persons[ids.ID_PERSON_BOB] = Bob
+// TODO Rename to index.js
 
-module.exports = persons
+function initPersons() {
+    let persons = {};
+    for (const name of Object.values(ids)) {
+        persons[name] = new Person(name);
+    }
+    return persons;
+};
+
+module.exports = { initPersons };
