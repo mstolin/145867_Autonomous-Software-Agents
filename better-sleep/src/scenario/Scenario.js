1ca43a04add895ccd247608da6df2b5f09f72997
Marcel Stolin <mstolin@mailbox.org>
2022-08-04 15:39:51 +0200

    Removed everything unecessary from scenario file

--- better-sleep/src/scenario/Scenario.js
+++ better-sleep/src/scenario/Scenario.js
@@ -2,18 +2,7 @@
     IMPORTS
 */
 // World
-const house = require("./world/House");
-const personIds = require("./world/persons/PersonIds");
-const Persons = require("./world/persons/Persons");
 const roomIds = require("./world/rooms/RoomIds");
-// BDI
-const houseAgent = require("./bdi/agents/house-agent/HouseAgent");
-const shutterAgents = require("./bdi/agents/shutter-agent");
-const {
-    SenseMovementGoal,
-    SenseDaytimeGoal,
-    SenseIlluminanceGoal,
-} = require("./bdi/agents/house-agent/Goals");
 // Utils
 const Clock = require("../lib/utils/Clock");
 // Scenario
@@ -21,56 +10,50 @@ const executeRoutine = require("./Routine");
 // Observers
 const {
     observeAllRooms,
-    observeAllPersons,
+    observePersonLocations,
     observeHouseSensors,
 } = require("./observers");
 // Environment
-const { initEnvironment, turnOnSensors } = require("./Environment");
+const { initEnvironment } = require("./Environment");
 
 /*
-    OBSERVERS
-    For debugging purposes.
+    HELPER FUNCTIONS
 */
-observeAllRooms();
-observeAllPersons();
-observeHouseSensors();
+function triggerMotionSensors(house) {
+    return new Promise((resolve, _) => {
+        let room = house.getRoom(roomIds.ID_ROOM_BEDROOM);
+        let persons = Object.values(house.persons);
+        for (person of persons) {
+            room.addResident(person);
+        }
+        resolve(house);
+    });
+}
 
-/*
-    HOUSE AGENT SENSOR INTENTIONS
-*/
-houseAgent.postSubGoal(
-    new SenseMovementGoal({
-        persons: [
-            house.getPerson(personIds.ID_PERSON_SANDRA),
-            house.getPerson(personIds.ID_PERSON_BOB),
-        ],
-    })
-);
-houseAgent.postSubGoal(new SenseIlluminanceGoal());
-houseAgent.postSubGoal(new SenseDaytimeGoal());
+function startObservers(house) {
+    return new Promise((resolve, _) => {
+        observeAllRooms(house);
+        observePersonLocations(house);
+        observeHouseSensors(house);
+        resolve(house);
+    });
+}
 
 /*
     ROUTINE
 */
-// Start the routine when the clock starts
-Clock.global.observe("mm", () => {
-    let time = Clock.global;
-    executeRoutine(time);
-});
 initEnvironment()
-    .then((_) => {
-        turnOnSensors();
-    })
-    .then((_) => {
-        // trigger motion sensor for inital locations
-        let room = house.getRoom(roomIds.ID_ROOM_BEDROOM);
-        for (person of Object.values(Persons)) {
-            room.addResident(person);
-        }
-    })
-    .then((_) => {
+    .then((house) => startObservers(house))
+    .then((house) => triggerMotionSensors(house))
+    .then((house) => {
+        // Start the routine when the clock starts
+        Clock.global.observe("mm", () => {
+            let time = Clock.global;
+            executeRoutine(time, house);
+        });
         // To simplify things, the day starts at 5 in the morning
         Clock.global.set("hh", 5);
         // Start the clock
         Clock.startTimer(1);
-    });
+    })
+    .catch((err) => console.error("ERROR", err));
