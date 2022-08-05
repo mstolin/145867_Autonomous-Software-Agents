a2ad61bdd1d7cff10b4831daf9147f08e9a7210b
Marcel Stolin <mstolin@mailbox.org>
2022-08-05 12:30:34 +0200

    Fixed way to initialize light agents

--- better-sleep/src/scenario/bdi/agents/light-agent/index.js
+++ better-sleep/src/scenario/bdi/agents/light-agent/index.js
@@ -1,13 +1,22 @@
 const LightAgent = require("../../../../lib/bdi/LightAgent");
 const { initIntentions } = require("./intentions");
 
-const initLightAgent = (house) => {
-    let intentions = initIntentions(house);
-    let agent = new LightAgent("Light-Agent", house.rooms);
+function initLightAgent(room) {
+    let agent = new LightAgent(`${room.name}-LightAgent`, room);
+    let intentions = initIntentions(room.name);
     for (const intention of intentions) {
         agent.intentions.push(intention);
     }
     return agent;
-};
+}
 
-module.exports = { initLightAgent };
+function initLightAgents(house) {
+    let lightAgents = Object.keys(house.rooms).map((roomId) => {
+        let room = house.getRoom(roomId);
+        let agent = initLightAgent(room);
+        return { [roomId]: agent };
+    });
+    return Object.assign({}, ...lightAgents);
+}
+
+module.exports = { initLightAgents };
